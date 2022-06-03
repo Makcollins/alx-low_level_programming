5fb35f325bbe82147edc057b00aacfc35d7d4a76
Makcollins <makcollins16@gmail.com>
2022-06-03 11:31:59 +0000

    printing in c using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/*c program that prints using put function
+	 *
+	 */
+puts("\"Progrmming is like buiding a multilingual puzzle");
+getchar();
+return (0);
+}
