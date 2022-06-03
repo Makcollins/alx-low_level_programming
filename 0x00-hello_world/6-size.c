80800483f1ab4a0a44812730514e68345eb4796f
Makcollins <makcollins16@gmail.com>
2022-06-03 12:08:09 +0000

    the c sizeof() operator

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+char c;
+int i;
+
+printf("Size of a char: %d \n", sizeof(c));
+printf("Size of an int: %d \n", sizeof(i));
+printf("Size of a long int: %d \n", sizeof(long int));
+printf("Size of a long long int: %d \n", sizeof(long long int));
+printf("Size of a float: %d \n", sizeof(float));
+}
