8c0382f88a29c1efcf9b8a087223d84760eafacb
Makcollins <makcollins16@gmail.com>
2022-06-06 20:59:23 +0000

    single digit decimal numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int x= 0;
+    for (x = 0; x < 10 ; x++)
+    {
+        printf("%d",x);
+       
+    }
+    
+    putchar('\n');
+    return 0;
+}
