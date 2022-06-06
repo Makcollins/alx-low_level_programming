e855c203a807e29289185536feb322430ec34daf
Makcollins <makcollins16@gmail.com>
2022-06-06 21:15:12 +0000

    reversed alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int x= 'a';
+    for (x = 'z'; x >='a' ; x--)
+    {
+        putchar(x);
+       
+    }
+    
+    putchar('\n');
+    return 0;
+}
