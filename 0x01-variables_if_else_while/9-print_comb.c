142eb1a37cefdab71887b5ac0fa786735d1c43bc
Makcollins <makcollins16@gmail.com>
2022-06-07 03:17:55 +0000

    print combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int x= '0';
+    for (x = '0'; x <='9' ; x++)
+        {
+        putchar(x);
+     	 putchar(',');
+        putchar('\t');
+							             
+        }
+		        
+        putchar('\n');
+    return 0;
+}
