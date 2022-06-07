0e41bcc4a3f5f54c4e29e6dc432def5f167d5c87
Makcollins <makcollins16@gmail.com>
2022-06-07 03:51:09 +0000

    combination of 3s

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+		int x= '0';
+			int y= '0';
+				int z= '0';
+				    for (x = '0'; x <='9' ; x++)
+					        {
+							    	for (y='0'; y<='9'; y++)
+											{
+												    		for (z='0'; z<='9'; z++)
+																		{
+																							putchar(x);
+																											putchar(y);
+																											    			putchar(z);
+																														    			putchar(',');
+																																	    			putchar('\t');
+																																							}
+														    		
+																}
+								        
+								       
+								    }
+				        
+				        putchar('\n');
+					    return 0;
+}
